0de92fbbdd8e8dab9f0b75a2ae953ad725743e74
Shirley Wu <shirleyxueyangwu@gmail.com>
2019-09-26 15:39:41 -0400

    Start project with index.html, index.js, App.vue and d3, lodash

---
+++ components/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+import * as d3 from 'd3'
+export default {
+  name: 'app',
+  },
+}
+</script>
+
+<style scoped>
+</style>
