19ac40cd2984193d78aac675b12cb535941c3f44
Shirley Wu <shirleyxueyangwu@gmail.com>
2019-09-30 18:14:28 -0400

    organize in a grid

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <svg :width='width' :height='height' @mousemove='onHover'>
+    <svg :width='width' :height='height'>
       <!-- gooey effect -->
       <defs>
          <filter id="gooey">
@@ -14,12 +14,14 @@
           <circle v-for='c in d.colors' :cx='c.x' :cy='c.y' :r='c.r' :fill='c.color' />
         </g>
         <text text-anchor='middle' dy='.35em'>{{ d.name }}</text>
+        <circle :r='1.25 * largest' opacity='0'
+          @mouseenter='hovered = d' @mouseleave='hovered = null' />
       </g>
     </svg>
     <!-- IMAGE -->
     <div v-if='hovered' :style='{
       position: `absolute`,
-      top: hovered.y + 2.5 * largest,
+      top: hovered.y + 1.75 * largest,
       left: hovered.x,
       transform: `translate(-50%, 0)`,
     }'>
@@ -35,7 +37,7 @@ import chroma from 'chroma-js'
 import data from '../data/studentsWithPhotos.json'
 import images from '../data/photos/*/*.*'
 
-const largest = 24
+const largest = 40
 const margin = {left: 20, top: 20, right: 20, bottom: 20}
 
 export default {
@@ -44,7 +46,7 @@ export default {
     return {
       width: window.innerWidth,
       height: window.innerHeight,
-      year: 2011,
+      year: 2001,
       years: [],
       students: [],
       hovered: null,
@@ -52,14 +54,15 @@ export default {
     }
   },
   mounted() {
-    this.yScale = d3.scaleLinear().domain([0, 1]).range([1.5 * largest, -1.5 * largest])
-    this.sizeScale = d3.scaleLinear().domain([0, 4]).range([largest, largest / 2])
+    this.yScale = d3.scaleLinear().domain([0, 1]).range([0.5 * largest, -0.5 * largest])
+    this.sizeScale = d3.scaleLog().domain([1, 5]).range([largest, largest / 10])
     this.colorSimulation = d3.forceSimulation()
       .force('x', d3.forceX(0))
       .force('y', d3.forceY(0))
-      .force('collide', d3.forceCollide(d => d.r / 2))
+      .force('collide', d3.forceCollide(d => d.r * 0.5))
       .stop()
 
+    const perRow = Math.floor(this.width / (largest * 3))
     this.students = _.chain(data)
       .filter(d => +d.year === this.year && d.colors)
       .map((d, i) => {
@@ -67,45 +70,25 @@ export default {
           const lightness = chroma(color).hsl()[2]
           return {
             color,
-            x: _.random(-1.5 * largest, 1.5 * largest),
+            x: _.random(-0.5 * largest, 0.5 * largest),
             y: this.yScale(lightness),
-            r: this.sizeScale(i),
+            r: this.sizeScale(i + 1),
           }
         })
         // simulate
         this.colorSimulation.nodes(colors).alpha(1)
         _.times(300, this.colorSimulation.tick())
 
-        const [hue, saturation, lightness] = chroma.average(d.colors, 'hsl').hsl()
-        let x = ((hue || 0 + 120) % 360) / 360
-        x = x * this.width * 0.2 + this.width * 0.4
-        const y = (1 - lightness) * this.height * 0.1 + this.height * 0.45
+        const x = ((i % perRow) + 0.5) * 3 * largest
+        const y = (Math.floor(i / perRow) + 0.5) * 3 * largest
         return {
           colors,
           name: d.name.split(' ')[0],
           x, y,
           image: _.values(images[d.year][d.netID])[0],
-          // forceX: x, forceY: y,
-          // x: _.random(this.width * 0.4, this.width * 0.6),
-          // y: _.random(this.height * 0.45, this.height * 0.55),
         }
       }).value()
 
-    this.simulation = d3.forceSimulation(this.students)
-      .force('center', d3.forceCenter(this.width / 2, this.height / 2))
-      // .force('x', d3.forceX(d => d.forceX))
-      // .force('y', d3.forceY(d => d.forceY))
-      .force('collide', d3.forceCollide(2 * largest))
-      // .force('charge', d3.forceManyBody())
-  },
-  methods: {
-    onHover(e) {
-      const {clientX, clientY} = e
-      const hovered = this.simulation.find(clientX, clientY, 2 * largest)
-      if (hovered !== this.hovered) {
-        this.hovered = hovered
-      }
-    },
   },
 }
 </script>
@@ -113,5 +96,6 @@ export default {
 <style scoped>
 text {
   font-size: 12px;
+  fill: #fff;
 }
 </style>
